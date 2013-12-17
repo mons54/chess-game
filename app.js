99cc4c80ba6d8c47c7d7ebbaa3a2c567ee7dab4f
mons54 <j.mons54@gmail.com>
2013-12-17 02:50:56 +0100

    change app module & global

--- app.js
+++ app.js
@@ -8,21 +8,20 @@ app.root = __dirname;
 
 global.appId = '459780557396952';
 global.secret = 'ffba6ba90d75f0e2ffd73d946fd5f1bd';
+
 global.host = 'chess-game.herokuapp.com';
 global.redirectUri = 'https://apps.facebook.com/the-chess-game/';
 global.root = path.resolve('/');
+global.port = process.env.PORT || 3000;
 
-global.graph = require('fbgraph');
-
-var port = process.env.PORT || 3000;
-
-global.socket = require('socket.io').listen(app.listen(port));
-
-global.socket.set('log level', 1);
-global.socket.set('origins', global.host + ':*');
-
+global.io = require('socket.io').listen(app.listen(port));
+global.fbgraph = require('fbgraph');
 global.mongoose = require('mongoose');
-global.mongoose.connect('mongodb://mons54:jsOL160884@jack.mongohq.com:10019/chess');
+
+io.set('log level', 1);
+io.set('origins', host + ':*');
+
+mongoose.connect('mongodb://mons54:jsOL160884@jack.mongohq.com:10019/chess');
 
 require('./app/server/modules/mongoose');
 require('./app/server/modules/socket');
