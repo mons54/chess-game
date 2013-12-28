2b71d896b5284eef3dd455a8345b831a96f41e4a
mons54 <j.mons54@gmail.com>
2013-12-28 20:07:41 +0100

    update router

--- app/server/router.js
+++ app/server/router.js
@@ -2,6 +2,12 @@
 module.exports = function(app) {
 
 	app.all('/', function(req, res) {
+		
+		if (req.headers.host != global.host) {
+			res.redirect(global.redirectUri);
+			return;
+		}
+		
 		res.render('index', { title: 'Chess Game'});
 	});
 	
