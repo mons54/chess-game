66aabdc7e28819ea71b88ba39f3e3af20288d30d
Julien Simonet <j.mons54@gmail.com>
2014-05-11 18:45:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var path = require('path'),
 
 app.root = __dirname;
 
-global.appId = '459780557396952';
-global.secret = 'ffba6ba90d75f0e2ffd73d946fd5f1bd';
+global.appId = '466889913406471';
+global.secret = '61f10f2183f931b1d2db51d3711919b8';
 
-global.host = 'chess-game.jit.su';
-global.redirectUri = 'https://apps.facebook.com/the-chess-game/';
+global.host = 'jsimonet.ma-luna.net';
+global.redirectUri = 'https://apps.facebook.com/____test/';
 global.root = path.resolve('/');
 global.port = process.env.PORT || 3000;
 
@@ -21,7 +21,7 @@ global.mongoose = require('mongoose');
 io.set('log level', 1);
 io.set('origins', host + ':*');
 
-mongoose.connect('mongodb://mons54:swOLjsfb162028@candidate.14.mongolayer.com:10036/chess_new');
+mongoose.connect('mongodb://mons54:gTu59JsUOp975s55sTtPmQze@oceanic.mongohq.com:10096/chess');
 
 require('./app/server/modules/mongoose');
 require('./app/server/modules/socket');
