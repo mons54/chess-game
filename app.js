b4d5492852b37b7a185c18c85da3826559f580bd
mons54 <j.mons54@gmail.com>
2014-05-29 20:53:18 +0200

    update app.js & package.json

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
+var express		= require('express'),
+	connect		= require('connect'),
+	mongoose	= require('mongoose'),
+	app			= express(),
+	io			= require('socket.io').listen(app.listen(process.env.PORT || 3000)),
+	fbgraph 	= require('fbgraph');
 
-var path = require('path'),
-	express = require('express'),
-	app = express(),
-	connect = require('connect');
+global.dirname 	= __dirname;
 
-app.root = __dirname;
+require(dirname + '/server/config')(app, express, connect, mongoose, io);
 
-global.appId = '466889913406471';
-global.secret = '61f10f2183f931b1d2db51d3711919b8';
-
-global.host = 'jsimonet.ma-luna.net';
-global.redirectUri = 'https://apps.facebook.com/____test/';
-global.root = path.resolve('/');
-global.port = process.env.PORT || 3000;
-
-global.io = require('socket.io').listen(app.listen(port));
-global.fbgraph = require('fbgraph');
-global.mongoose = require('mongoose');
-
-io.set('log level', 1);
-io.set('origins', host + ':*');
-
-mongoose.connect('mongodb://mons54:gTu59JsUOp975s55sTtPmQze@oceanic.mongohq.com:10096/chess');
-
-require('./app/server/modules/mongoose');
-require('./app/server/modules/socket');
-
-require('./app/config')(app, express, connect);
-
-require('./app/server/payments')(app);
-require('./app/server/sponsorpay')(app);
-require('./app/server/tokenads')(app);
-require('./app/server/router')(app);
+require(dirname + '/server/modules/mongoose')(mongoose);
+require(dirname + '/server/modules/socket')(io, mongoose);
