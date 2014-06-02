0e8e48b8beee4941af5357fb461aaf0cd8e91e23
Julien Simonet <j.mons54@gmail.com>
2014-06-02 15:17:03 +0200

    Update models.js

--- server/modules/mongoose/models.js
+++ server/modules/mongoose/models.js
@@ -1,11 +1,11 @@
 module.exports = function (mongoose) {
-	return {
-		users 			: mongoose.model('users', mongoose.collections.users),
-		games 			: mongoose.model('parties', mongoose.collections.games),
-		badges 			: mongoose.model('user_badges', mongoose.collections.badges),
-		freeTokens		: mongoose.model('free_tokens', mongoose.collections.freeTokens),
-		payments		: mongoose.model('paiements', mongoose.collections.payments),
-		sponsorPay 		: mongoose.model('sponsor_pays', mongoose.collections.sponsorPay),
-		tokenAds 		: mongoose.model('token_ads', mongoose.collections.tokenAds)
-	};
+    return {
+        users: mongoose.model('users', mongoose.collections.users),
+        games: mongoose.model('parties', mongoose.collections.games),
+        badges: mongoose.model('user_badges', mongoose.collections.badges),
+        freeTokens: mongoose.model('free_tokens', mongoose.collections.freeTokens),
+        payments: mongoose.model('paiements', mongoose.collections.payments),
+        sponsorPay: mongoose.model('sponsor_pays', mongoose.collections.sponsorPay),
+        tokenAds: mongoose.model('token_ads', mongoose.collections.tokenAds)
+    };
 };
