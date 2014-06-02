4ffe004fe82ff8ca4303799d7345bf0d3be798e0
Julien Simonet <j.mons54@gmail.com>
2014-06-02 15:14:18 +0200

    Update sponsorpay.js

--- server/modules/sponsorpay.js
+++ server/modules/sponsorpay.js
@@ -1,242 +1,260 @@
-module.exports = function(app, mongoose) {
+module.exports = function (app, mongoose) {
 
-	var security_token = "911f3fd471bdb649c9beb94631edf75a";
-	
-	var users = mongoose.models.users,
-		sponsorpay = mongoose.models.sponsorPay;
+    var security_token = "911f3fd471bdb649c9beb94631edf75a";
 
-	app.get('/sponsorpay', function(req, res) {
-	
-		var response = "HTTP/1.0 400 Bad Request: wrong SID";
-		
-		var url = req.url.split('/');
-		
-		if(url[1]) {
-			
-			url = url[1].replace("sponsorpay?", "");
-			
-			var sid = get_param('sid', url);
-			var amount = get_param('amount', url);
-			var uid = get_param('uid', url);
-			var transid = get_param('_trans_id_', url);
-			
-			if(sid && amount && uid && transid) {
-				
-				var sha1_of_important_data = sha1(security_token + uid + amount + transid); 
-				
-				if (sid == sha1_of_important_data) {
-				
-					response = "HTTP200";
-					
-					sponsorpay.count({id:transid}, function (err, nb) {
-						
-						if (err || nb) return;
-						
-						var trans = new sponsorpay({ id : transid });
-						trans.uid = uid;
-						trans.amount = amount;
-						
-						trans.save();
-						
-						users.find({uid:uid}, function (err, data) {
-								
-							if (err || !data[0]) return;
-							
-							var token = parseInt(data[0].tokens) + parseInt(amount);
-						
-							users.update({uid: uid }, { $set: {tokens: token} }, function (err) { });
-						});
-					});
-				}
-			}
-		}
-		
-		res.send(response);
-	});
-	
-	function get_param(param, url) {
+    var users = mongoose.models.users,
+        sponsorpay = mongoose.models.sponsorPay;
 
-		var variables = url.split('&');
+    app.get('/sponsorpay', function (req, res) {
 
-		for (var i = 0; i < variables.length; i++) {
-			var name = variables[i].split('=');
+        var response = "HTTP/1.0 400 Bad Request: wrong SID";
 
-			if (name[0] == param) {
-				return name[1];
-			}
-		}
-	}
-	
-	function sha1 (str) {
-	  
-		var rotate_left = function (n, s) {
-			var t4 = (n << s) | (n >>> (32 - s));
-			return t4;
-		};
+        var url = req.url.split('/');
 
-		var cvt_hex = function (val) {
-			var str = "";
-			var i;
-			var v;
+        if (url[1]) {
 
-			for (i = 7; i >= 0; i--) {
-				v = (val >>> (i * 4)) & 0x0f;
-				str += v.toString(16);
-			}
-			return str;
-		};
+            url = url[1].replace("sponsorpay?", "");
 
-		var blockstart;
-		var i, j;
-		var W = new Array(80);
-		var H0 = 0x67452301;
-		var H1 = 0xEFCDAB89;
-		var H2 = 0x98BADCFE;
-		var H3 = 0x10325476;
-		var H4 = 0xC3D2E1F0;
-		var A, B, C, D, E;
-		var temp;
+            var sid = get_param('sid', url);
+            var amount = get_param('amount', url);
+            var uid = get_param('uid', url);
+            var transid = get_param('_trans_id_', url);
 
-		str = utf8_encode(str);
-		var str_len = str.length;
+            if (sid && amount && uid && transid) {
 
-		var word_array = [];
-		for (i = 0; i < str_len - 3; i += 4) {
-			j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 | str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
-			word_array.push(j);
-		}
+                var sha1_of_important_data = sha1(security_token + uid + amount + transid);
 
-		switch (str_len % 4) {
-			case 0: i = 0x080000000; break;
-			case 1: i = str.charCodeAt(str_len - 1) << 24 | 0x0800000; break;
-			case 2: i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000; break;
-			case 3: i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) << 8 | 0x80; break;
-		}
+                if (sid == sha1_of_important_data) {
 
-		word_array.push(i);
+                    response = "HTTP200";
 
-		while ((word_array.length % 16) != 14) {
-			word_array.push(0);
-		}
+                    sponsorpay.count({
+                        id: transid
+                    }, function (err, nb) {
 
-		word_array.push(str_len >>> 29);
-		word_array.push((str_len << 3) & 0x0ffffffff);
+                        if (err || nb) return;
 
-		for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
-			for (i = 0; i < 16; i++) {
-				W[i] = word_array[blockstart + i];
-			}
-			for (i = 16; i <= 79; i++) {
-				W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
-			}
+                        var trans = new sponsorpay({
+                            id: transid
+                        });
+                        trans.uid = uid;
+                        trans.amount = amount;
+
+                        trans.save();
+
+                        users.find({
+                            uid: uid
+                        }, function (err, data) {
+
+                            if (err || !data[0]) return;
+
+                            var token = parseInt(data[0].tokens) + parseInt(amount);
+
+                            users.update({
+                                uid: uid
+                            }, {
+                                $set: {
+                                    tokens: token
+                                }
+                            }, function (err) {});
+                        });
+                    });
+                }
+            }
+        }
+
+        res.send(response);
+    });
+
+    function get_param(param, url) {
+
+        var variables = url.split('&');
+
+        for (var i = 0; i < variables.length; i++) {
+            var name = variables[i].split('=');
+
+            if (name[0] == param) {
+                return name[1];
+            }
+        }
+    }
+
+    function sha1(str) {
+
+        var rotate_left = function (n, s) {
+            var t4 = (n << s) | (n >>> (32 - s));
+            return t4;
+        };
+
+        var cvt_hex = function (val) {
+            var str = "";
+            var i;
+            var v;
+
+            for (i = 7; i >= 0; i--) {
+                v = (val >>> (i * 4)) & 0x0f;
+                str += v.toString(16);
+            }
+            return str;
+        };
+
+        var blockstart;
+        var i, j;
+        var W = new Array(80);
+        var H0 = 0x67452301;
+        var H1 = 0xEFCDAB89;
+        var H2 = 0x98BADCFE;
+        var H3 = 0x10325476;
+        var H4 = 0xC3D2E1F0;
+        var A, B, C, D, E;
+        var temp;
+
+        str = utf8_encode(str);
+        var str_len = str.length;
+
+        var word_array = [];
+        for (i = 0; i < str_len - 3; i += 4) {
+            j = str.charCodeAt(i) << 24 | str.charCodeAt(i + 1) << 16 | str.charCodeAt(i + 2) << 8 | str.charCodeAt(i + 3);
+            word_array.push(j);
+        }
+
+        switch (str_len % 4) {
+        case 0:
+            i = 0x080000000;
+            break;
+        case 1:
+            i = str.charCodeAt(str_len - 1) << 24 | 0x0800000;
+            break;
+        case 2:
+            i = str.charCodeAt(str_len - 2) << 24 | str.charCodeAt(str_len - 1) << 16 | 0x08000;
+            break;
+        case 3:
+            i = str.charCodeAt(str_len - 3) << 24 | str.charCodeAt(str_len - 2) << 16 | str.charCodeAt(str_len - 1) << 8 | 0x80;
+            break;
+        }
+
+        word_array.push(i);
+
+        while ((word_array.length % 16) != 14) {
+            word_array.push(0);
+        }
+
+        word_array.push(str_len >>> 29);
+        word_array.push((str_len << 3) & 0x0ffffffff);
+
+        for (blockstart = 0; blockstart < word_array.length; blockstart += 16) {
+            for (i = 0; i < 16; i++) {
+                W[i] = word_array[blockstart + i];
+            }
+            for (i = 16; i <= 79; i++) {
+                W[i] = rotate_left(W[i - 3] ^ W[i - 8] ^ W[i - 14] ^ W[i - 16], 1);
+            }
 
 
-			A = H0;
-			B = H1;
-			C = H2;
-			D = H3;
-			E = H4;
+            A = H0;
+            B = H1;
+            C = H2;
+            D = H3;
+            E = H4;
 
-			for (i = 0; i <= 19; i++) {
-				temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B, 30);
-				B = A;
-				A = temp;
-			}
+            for (i = 0; i <= 19; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (~B & D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
+                E = D;
+                D = C;
+                C = rotate_left(B, 30);
+                B = A;
+                A = temp;
+            }
 
-			for (i = 20; i <= 39; i++) {
-				temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B, 30);
-				B = A;
-				A = temp;
-			}
+            for (i = 20; i <= 39; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
+                E = D;
+                D = C;
+                C = rotate_left(B, 30);
+                B = A;
+                A = temp;
+            }
 
-			for (i = 40; i <= 59; i++) {
-				temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B, 30);
-				B = A;
-				A = temp;
-			}
+            for (i = 40; i <= 59; i++) {
+                temp = (rotate_left(A, 5) + ((B & C) | (B & D) | (C & D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
+                E = D;
+                D = C;
+                C = rotate_left(B, 30);
+                B = A;
+                A = temp;
+            }
 
-			for (i = 60; i <= 79; i++) {
-				temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-				E = D;
-				D = C;
-				C = rotate_left(B, 30);
-				B = A;
-				A = temp;
-			}
+            for (i = 60; i <= 79; i++) {
+                temp = (rotate_left(A, 5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
+                E = D;
+                D = C;
+                C = rotate_left(B, 30);
+                B = A;
+                A = temp;
+            }
 
-			H0 = (H0 + A) & 0x0ffffffff;
-			H1 = (H1 + B) & 0x0ffffffff;
-			H2 = (H2 + C) & 0x0ffffffff;
-			H3 = (H3 + D) & 0x0ffffffff;
-			H4 = (H4 + E) & 0x0ffffffff;
-		}
+            H0 = (H0 + A) & 0x0ffffffff;
+            H1 = (H1 + B) & 0x0ffffffff;
+            H2 = (H2 + C) & 0x0ffffffff;
+            H3 = (H3 + D) & 0x0ffffffff;
+            H4 = (H4 + E) & 0x0ffffffff;
+        }
 
-		temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
-		return temp.toLowerCase();
-	}
-	
-	function utf8_encode (argString) {
-		if (argString === null || typeof argString === "undefined") {
-			return "";
-		}
+        temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
+        return temp.toLowerCase();
+    }
 
-		var string = (argString + '');
-		var utftext = '',
-		start, end, stringl = 0;
+    function utf8_encode(argString) {
+        if (argString === null || typeof argString === "undefined") {
+            return "";
+        }
 
-		start = end = 0;
-		stringl = string.length;
-		for (var n = 0; n < stringl; n++) {
-			var c1 = string.charCodeAt(n);
-			var enc = null;
+        var string = (argString + '');
+        var utftext = '',
+            start, end, stringl = 0;
 
-			if (c1 < 128) {
-				end++;
-			} else if (c1 > 127 && c1 < 2048) {
-				enc = String.fromCharCode(
-				(c1 >> 6)        | 192,
-				( c1        & 63) | 128
-				);
-			} else if (c1 & 0xF800 != 0xD800) {
-				enc = String.fromCharCode(
-				(c1 >> 12)       | 224,
-				((c1 >> 6)  & 63) | 128,
-				( c1        & 63) | 128
-				);
-			} else {
-				if (c1 & 0xFC00 != 0xD800) { throw new RangeError("Unmatched trail surrogate at " + n); }
-				var c2 = string.charCodeAt(++n);
-				if (c2 & 0xFC00 != 0xDC00) { throw new RangeError("Unmatched lead surrogate at " + (n-1)); }
-				c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
-				enc = String.fromCharCode(
-				(c1 >> 18)       | 240,
-				((c1 >> 12) & 63) | 128,
-				((c1 >> 6)  & 63) | 128,
-				( c1        & 63) | 128
-				);
-			}
-			if (enc !== null) {
-				if (end > start) {
-					utftext += string.slice(start, end);
-				}
-				utftext += enc;
-				start = end = n + 1;
-			}
-		}
+        start = end = 0;
+        stringl = string.length;
+        for (var n = 0; n < stringl; n++) {
+            var c1 = string.charCodeAt(n);
+            var enc = null;
 
-		if (end > start) {
-			utftext += string.slice(start, stringl);
-		}
+            if (c1 < 128) {
+                end++;
+            } else if (c1 > 127 && c1 < 2048) {
+                enc = String.fromCharCode(
+                    (c1 >> 6) | 192, (c1 & 63) | 128
+                );
+            } else if (c1 & 0xF800 != 0xD800) {
+                enc = String.fromCharCode(
+                    (c1 >> 12) | 224, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                );
+            } else {
+                if (c1 & 0xFC00 != 0xD800) {
+                    throw new RangeError("Unmatched trail surrogate at " + n);
+                }
+                var c2 = string.charCodeAt(++n);
+                if (c2 & 0xFC00 != 0xDC00) {
+                    throw new RangeError("Unmatched lead surrogate at " + (n - 1));
+                }
+                c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
+                enc = String.fromCharCode(
+                    (c1 >> 18) | 240, ((c1 >> 12) & 63) | 128, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                );
+            }
+            if (enc !== null) {
+                if (end > start) {
+                    utftext += string.slice(start, end);
+                }
+                utftext += enc;
+                start = end = n + 1;
+            }
+        }
 
-		return utftext;
-	}
+        if (end > start) {
+            utftext += string.slice(start, stringl);
+        }
+
+        return utftext;
+    }
 };
