353b5cbc34ac64b12062d80ff1e62666edd90c49
Julien Simonet <j.mons54@gmail.com>
2014-06-02 15:13:09 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,17 +1,17 @@
 module.exports = function (app, express, connect, mongoose, io) {
 
-	app.facebook = {
-		appId		: '466889913406471',
-		secret		: '61f10f2183f931b1d2db51d3711919b8',
-		redirectUri	: 'https://apps.facebook.com/____test/'
-	};
+    app.facebook = {
+        appId: '466889913406471',
+        secret: '61f10f2183f931b1d2db51d3711919b8',
+        redirectUri: 'https://apps.facebook.com/____test/'
+    };
 
-	app.use(express.static(dirname + '/public'));
-	app.use(require('body-parser')());
+    app.use(express.static(dirname + '/public'));
+    app.use(require('body-parser')());
 
 
-        mongoose.connect('mongodb://127.0.0.1:27017/chess_new');
+    mongoose.connect('mongodb://127.0.0.1:27017/chess_new');
 
-        io.set('origins', 'mons54.parthuisot.fr:*');
+    io.set('origins', 'mons54.parthuisot.fr:*');
 
 };
