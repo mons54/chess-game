17072eea7e0bcd13226b6af87390c67f8cc650f1
Julien Simonet <j.mons54@gmail.com>
2014-06-02 15:16:25 +0200

    Update mongoose.js

--- server/modules/mongoose.js
+++ server/modules/mongoose.js
@@ -1,4 +1,4 @@
 module.exports = function (mongoose) {
-	mongoose.collections = require(dirname + '/server/modules/mongoose/collections')(mongoose);
-	mongoose.models = require(dirname + '/server/modules/mongoose/models')(mongoose);
+    mongoose.collections = require(dirname + '/server/modules/mongoose/collections')(mongoose);
+    mongoose.models = require(dirname + '/server/modules/mongoose/models')(mongoose);
 };
