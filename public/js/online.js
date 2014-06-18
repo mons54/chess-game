fb48947445ef7b17e31cc6c80b9bceaf4cfdba25
mons54 <j.mons54@gmail.com>
2014-06-18 20:58:02 +0200

    add ban

--- public/js/online.js
+++ public/js/online.js
@@ -245,6 +245,10 @@
                 return false;
 
             }.bind(this));
+
+            
+
+            this.banUser = $('<div/>').appendTo(left);
         },
 
         _create_column_right: function () {
@@ -1405,6 +1409,10 @@
 
             this.moderateur = data.moderateur;
 
+            if (this.moderateur) {
+                this._ban_user();
+            }
+
             this.tokens = {
                 ready: true,
                 data: token
@@ -1422,6 +1430,18 @@
             $(this.options.tokens).empty().text(token);
 
             this.free_time = data.free;
+        },
+
+        _ban_user: function () {
+            var uid = $('<input type="text">').appendTo(this.banUser);
+            var ban = $('<input type="checkbox" checked>').appendTo(this.banUser);
+            $('<button>OK</button>').appendTo(this.banUser).click(function () {
+                
+                this.socket.emit('banUser', {
+                    uid: uid.val(),
+                    ban: ban.val() == 'on' ? true : false
+                });
+            }.bind(this));
         },
 
         _free: function () {
